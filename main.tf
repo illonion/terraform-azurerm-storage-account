c4100408792fe708805fa8bc4f9d83d11b5f3bce
Fairy Bread <60532041+illonion@users.noreply.github.com>
2026-03-11 10:55:16 +1100

    Refactor main.tf to remove random_string resource

--- main.tf
+++ main.tf
@@ -1,46 +1,27 @@
-resource "random_string" "random" {
-  length  = 24
-  special = false
-  upper   = false
-}
-
 resource "azurerm_storage_account" "this" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  # ---- All hardcoded for testing ----
-  account_tier             = "Standard"
-  account_replication_type = "LRS"      # cheapest for testing
+  # --- Hardcoded values ---
   account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
   access_tier              = "Hot"
 
-  # Security
   min_tls_version                 = "TLS1_2"
   https_traffic_only_enabled      = true
   allow_nested_items_to_be_public = false
   shared_access_key_enabled       = true
-  public_network_access_enabled   = true  # true for easy testing
 
-  # Blob config
   blob_properties {
-    versioning_enabled = false
-
     delete_retention_policy {
       days = 7
     }
-
     container_delete_retention_policy {
       days = 7
     }
   }
 
-  # Tags
-  tags = merge(
-    {
-      ManagedBy   = "Terraform"
-      Environment = "Testing"
-    },
-    var.tags,
-  )
+  tags = var.tags
 }
