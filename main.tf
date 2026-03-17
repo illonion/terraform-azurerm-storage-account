12f108c98e135112301011565ba9c5597cf94dcd
Lawrence Zhou <zhou.lawrence.yz@gmail.com>
2026-03-17 13:29:15 +1100

    Many comments

--- main.tf
+++ main.tf
@@ -3,16 +3,27 @@ resource "azurerm_storage_account" "this" {
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  # --- Hardcoded values ---
+  # Hardcoded values
   account_kind             = "StorageV2"
+  // Valid options are - BlobStorage, BlockBlobStorage, FileStorage, Storage, QueueStorage, and StorageV2
+  // StorageV2 is the most versatile. FileStorage and QueueStorage are for specific use cases
   account_tier             = "Standard"
+  // Standard or Premium
   account_replication_type = "LRS"
+  // How do they want the data to be replicated?
+  // Valid options are - LRS, GRS, ZRS, GZRS, RAGRS, and RAGZRS
   access_tier              = "Hot"
+  // What access tier is wanted? - Hot, Cool, Cold 
 
   min_tls_version                 = "TLS1_2"
+  // TLS V1.2 is the minimum. Goes up to TLS 3.
   https_traffic_only_enabled      = true
+  // Prevents HTTP traffic
   allow_nested_items_to_be_public = false
+  // Allows or disallows nested items within this account to be public
   shared_access_key_enabled       = true
+  // IF you guys have a shared key, having the storage account would allow access via it.
+  // Otherwise they must be authorised with Azaure Active Directory (Azure AD)
 
   blob_properties {
     delete_retention_policy {
