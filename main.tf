848face04aa19f9aba3c200e2668d50efacd0b2f
Fairy Bread <60532041+illonion@users.noreply.github.com>
2026-04-13 10:58:42 +1000

    Add public network access setting to storage account
    Added public_network_access_enabled property to storage account resource.

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "azurerm_storage_account" "this" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  name                          = var.name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  public_network_access_enabled = var.public_network_access_enabled
 
   # Hardcoded values
   account_kind             = "StorageV2"
